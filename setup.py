656c7b2e08a3a75fb7ba384fea93bf8071589217
Almog Yalinewich <almog.yalin@gmail.com>
2022-01-14 18:32:49 -0500

    retouch setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='navier_stoked',
-        version='0.1',
-        packages=find_packages(exclude=['tests*']),
-        license='MIT',
-        description='Derives the Navier Stokes equations in different coordinate systems',
-        long_description=open('README.txt').read(),
-        install_requires=['sympy'],
-        url='https://github.com/bolverk/navier_stoked',
-        author='Almog Yalinewich',
-        author_email='almog.yalin@gmail.com'
-    )
+    name='navier_stoked',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Derives the Navier Stokes equations in different coordinate systems',
+    #long_description=open('README.txt').read(),
+    long_description='TBA',
+    install_requires=['sympy'],
+    url='https://github.com/bolverk/navier_stoked',
+    author='Almog Yalinewich',
+    author_email='almog.yalin@gmail.com'
+)
